8665ee2d2c9f46dbd25200935f82a6e73e9f974b
Vanessa Henson <ourmagicallife@gmail.com>
2016-04-09 14:06:38 -0600

    got the navigation working for mobile, and styled the top two sections

--- template-parts/skills.php
+++ template-parts/skills.php
@@ -1,29 +1,36 @@
 <div class="skills" ng-controller="skillsController">
-	<h1>My Skills</h1>
-	<ul ng-repeat="skillList in skills">
-	<li ng-repeat="skill in skillList">
-	{{ skill }}
-	</li>
-	</ul>
-
-	<div ng-show="highlights" ng-click="showSection( 'highlight-toggle', 'highlights' )" class="highlight-toggle" ><span>View</span> Highlights</div>
-	<div class="highlights">
-		<div ng-repeat="highlight in highlights">
-			<div class="name">{{ highlight.name }}</div>
-			<div class="description"> 
-				<div ng-repeat="description in highlight.description" >{{ description }}</div>
-			</div>
-		</div>
+	<h2>SKILLS I BRING TO YOU</h2>
+	<div class="skill-lists">
+		<ul ng-repeat="skillList in skills">
+		<li ng-repeat="skill in skillList">
+		{{ skill }}
+		</li>
+		</ul>
 	</div>
 
-	<div ng-show="education" ng-click="showSection( 'education-toggle', 'education' )" class="education-toggle"><span>View</span> Education</div>
-		<div class="education">
-			<div ng-repeat="edu in education">
-				<div class="year ng-click=">{{ edu.year }}</div>
-				<div class="field"> 
-					<div ng-repeat="field in edu.field">{{ field }}</div>
+	<div class="toggle-button" ng-show="highlights" ng-click="showSection( 'highlight-toggle', 'highlights' )" class="highlight-toggle" ><span>View</span> Highlights</div>
+	<br/>
+	<section>
+		<div class="highlights slide-content">
+			<div ng-repeat="highlight in highlights">
+				<div class="name">{{ highlight.name }}</div>
+				<div class="description"> 
+					<div ng-repeat="description in highlight.description" >{{ description }}</div>
 				</div>
 			</div>
 		</div>
-	</div>
+	</section>
+	<section>
+		<div class="toggle-button" ng-show="education" ng-click="showSection( 'education-toggle', 'education' )" class="education-toggle"><span>View</span> Education</div>
+		<br/>
+			<div class="education slide-content">
+				<div ng-repeat="edu in education">
+					<div class="year ng-click=">{{ edu.year }}</div>
+					<div class="field"> 
+						<div ng-repeat="field in edu.field">{{ field }}</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </div>
